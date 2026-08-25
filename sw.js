2472c2215ed45296f25218283332d5701613d0f7
silvaoo <silvaoo@users.noreply.github.com>
2026-08-25 05:52:22 +0000

    生駒をmaster.htmlから生成する方式へ移行 / 5アプリの骨組み統一が完了 / 版2.34 (by AIタケタニ)

--- sw.js
+++ sw.js
@@ -10,10 +10,11 @@
 
 // キャッシュ名にバージョンを入れておき、更新のたびにこの値を変えることで
 // 新しいService Workerが「更新あり」と判定されるようにする
-const CACHE_VERSION = 'ikoma-shukkin-v122';
+const CACHE_VERSION = 'ikoma-shukkin-v123';
 const CACHE_FILES = [
     './',
     './index.html',
+    './logs.html',      // 更新履歴。別ファイルに分けてある
     './dia-ikoma.json',        // ダイヤのデータ。圏外でも使えるよう控えておく
     './mascot.png',
     './icons/icon-192.png',
